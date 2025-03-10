529848aa65e49078e73ac29523ad15688273be77
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-10 15:36:30 -0400

    fixup! stone: Migrate away from TARGET_RECOVERY_DEVICE_MODULES
    * Dead as of QPR2.
    
    Change-Id: I05bcf246266b89fa7da964f7aa75870b5a63a3b9

--- device.mk
+++ device.mk
@@ -369,6 +369,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init_xiaomi_stone.recovery
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl
